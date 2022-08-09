09e2f90ad668fb84da71d24474b96b6dbfe76f9b
PeggyBevan <ucbtbev@ucl.ac.uk>
2022-08-09 14:45:21 -0700

    Adding line to remove n from filepath names before reading

--- unsupProj/dataset.py
+++ unsupProj/dataset.py
@@ -2,7 +2,6 @@
     Creating dataset class for cropped camera trap images.
     2022 Peggy Bevan 
 '''
-
 import os
 import json
 from torch.utils.data import Dataset
@@ -30,7 +29,8 @@ class CTDataset(Dataset):
 		Returns a single data point at given idx.
 		Here's where we actually load the image.
 		'''
-		image_path = os.path.join(self.base, self.img_list[idx])
+		filepath = img_list[idx].rstrip('\n') # remove \n from filepath
+		image_path = os.path.join(self.base, filepath)
 		# load image
 		img = Image.open(image_path).convert('RGB')     # the ".convert" makes sure we always get three bands in Red, Green, Blue order
 
@@ -38,3 +38,22 @@ class CTDataset(Dataset):
 		img_tensor = self.transform(img)
 
 		return img_tensor
+
+'''
+
+if __name__ = '__main__':
+
+	import yaml
+
+	with open('configs/cfg_resnet50.yaml', 'r') as f:
+		cfg = yaml.safe_load(f)
+	dataset = CTDataset(cfg, '../train.txt', None)
+
+	img_tensor = dataset.__getitem__(0)
+
+	# convert torch.Tensor back to PIL image
+	from torchvision.transforms import ToPILImage
+	img_pil = ToPILImage()(img_tensor)
+
+	img_pil.save('image.jpg')
+'''
