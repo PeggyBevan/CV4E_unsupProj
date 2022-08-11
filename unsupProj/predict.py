d9f947b305fee1da137582077e2ce35e57597664
PeggyBevan <ucbtbev@ucl.ac.uk>
2022-08-10 18:18:03 -0700

    uploading predict.py

--- unsupProj/predict.py
+++ unsupProj/predict.py
@@ -4,25 +4,31 @@
     2022 Peggy Bevan
 '''
 
-#load other scripts - must be in the same directory
-import model, dataset
+#load other scripts - must be in the same directory (unsupProj)
+import model, dataset, functions
+import yaml, json
+import pickle
 from model import CustomPegNet50
 from dataset import CTDataset
-
+from functions import create_dataloader, predict
 
 
 #create model and apply parameters
 model = CustomPegNet50()
 #this might cause an error if no GPU
-model.cuda
+#model.cuda
 
 # to call the fn
 img_list_path = '/home/pegbev/data/train.txt'
-cfg = 
+cfg = yaml.safe_load(open('/home/pegbev/CV4E_unsupProj/configs/cfg_resnet50.yaml'))
+#until GPU is bigger, num workers must be 2
+cfg['num_workers'] = 2
+cfg['device'] = 'cpu'
 dl = create_dataloader(cfg, img_list_path)
-#model = load_model(ckpt_path)
+
 prediction_dict = predict(cfg, dl, model)
 
 #write as json into filepath 
 
 
+pickle.dump(prediction_dict, open( "/home/pegbev/output/test_output.p", "wb" ))
