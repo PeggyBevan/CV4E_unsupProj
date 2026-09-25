8508bdf75a59836b5b0e81c913de8b0043e7af56
PeggyBevan <ucbtbev@ucl.ac.uk>
2026-09-25 12:39:55 +0100

    conda environment setup

---
+++ unsupProj/setup.sh
@@ -0,0 +1,8 @@
+conda create -n unsupProj python=3.10 -c conda-forge
+conda install numpy
+conda install pandas
+conda install matplotlib
+conda install json
+conda install pickle
+conda install seaborn
+hdbscan
